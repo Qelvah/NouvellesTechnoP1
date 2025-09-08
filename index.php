e59be49d8bcf7f98b8bca6a278713c5eb5cc9da9
Qelris <153022149+Qelvah@users.noreply.github.com>
2025-09-08 11:06:22 -0400

    test2

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require 'includes/header.php'; ?>
 
   <main>
-    ASGNGSBÉASGDBGASDNOSGAD
+    yodelayheehoo
     <img class="img_center" src="assets/images/bienvenue.png" alt="">
   </main>
   
