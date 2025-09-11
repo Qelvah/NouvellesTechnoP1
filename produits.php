785823846a87e7befbc1dd61844d3b3a89dd5b14
Mari0no1 <teamshadows07@gmail.com>
2025-09-10 23:29:00 -0400

    Makes the product page more responsive

--- produits.php
+++ produits.php
@@ -1,25 +1,25 @@
 <?php require 'includes/header.php'; ?>
 
-<main>
-  <h1>Produits</h1>
-  <div class="product_grid">
-    <div>
-      <img height=200 src="https://www.jaimefruitsetlegumes.ca/wp-content/uploads/2019/08/iStock_000007671231Large-e1565725651658-700x700.jpg">
-      <p>Bonne banane</p>
-    </div>
-    <div>
-      <img height=200 src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Big_flat_screwdriver.jpg">
-      <p>Tournevis noir</p>
-    </div>
-    <div>
-      <img height=200 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCK99dIgoRGb5SrDu5052mAclDZpvEfc4sYA&s">
-      <p>Chaise confortable</p>
-    </div>
-    <div>
-      <img height=200 src="https://img.netenders.com/@wordans/files/model_specifications/2022/10/26/1256228/1256228_lifestyle_big.jpg?width=593&timestamp=1686653814">
-      <p>Ballon à frapper avec le pied</p>
-    </div>
-  </div>
-</main>
+    <main>
+        <h1>Produits</h1>
+        <div class="product_grid">
+            <div>
+                <img src="https://www.jaimefruitsetlegumes.ca/wp-content/uploads/2019/08/iStock_000007671231Large-e1565725651658-700x700.jpg">
+                <p>Bonne banane</p>
+            </div>
+            <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Big_flat_screwdriver.jpg">
+                <p>Tournevis noir</p>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCK99dIgoRGb5SrDu5052mAclDZpvEfc4sYA&s">
+                <p>Chaise confortable</p>
+            </div>
+            <div>
+                <img src="https://img.netenders.com/@wordans/files/model_specifications/2022/10/26/1256228/1256228_lifestyle_big.jpg?width=593&timestamp=1686653814">
+                <p>Ballon à frapper avec le pied</p>
+            </div>
+        </div>
+    </main>
 
 <?php require 'includes/footer.php'; ?>
