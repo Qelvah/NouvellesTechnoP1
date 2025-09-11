92802a95656ac0b33958f9f294656d5e647951aa
Mari0no1 <teamshadows07@gmail.com>
2025-09-10 23:38:29 -0400

    Makes the contact page more responsive

--- contact.php
+++ contact.php
@@ -1,26 +1,26 @@
 <?php require 'includes/header.php'; ?>
 
-  <main>
-    <h2 class="contacth2">Entrez vos informations.</h1>
-    <form action="#" onsubmit="return onSubmit()">
-      <fieldset class="who">
-        <legend>Qui êtes-vous?</legend>
-        
-        <label for="prenom" class="prenom">Prénom: </label>
-        <input type="text" name="prenom" id="prenom" placeholder="Prénom">
-        <label for="nom" class="nom">Nom: </label>
-        <input type="text" name="nom" id="nom" placeholder="Nom">
-        <label for="email" class="email">Email: </label>
-        <input type="text" name="email" id="email" placeholder="email@domain.com">
-      </fieldset>
+    <main>
+        <h2 class="contacth2">Entrez vos informations.</h2>
+        <form class="form_contact" action="#" onsubmit="return onSubmit()">
+            <fieldset class="who">
+                <legend>Qui êtes-vous?</legend>
 
-      <fieldset class="raison">
-        <legend>Raison de contact?</legend>
-        <textarea name="message" id="message" placeholder="Écrire vos commentaires ici." class="textArea"></textarea>
-      </fieldset>
+                <label for="prenom" class="prenom">Prénom: </label>
+                <input type="text" name="prenom" id="prenom" placeholder="Prénom">
+                <label for="nom" class="nom">Nom: </label>
+                <input type="text" name="nom" id="nom" placeholder="Nom">
+                <label for="email" class="email">Email: </label>
+                <input type="text" name="email" id="email" placeholder="email@domain.com">
+            </fieldset>
 
-      <input class="submit" type="submit" value="Soumettre">
-    </form>
-  </main>
-  
+            <fieldset class="raison">
+                <legend>Raison de contact?</legend>
+                <textarea name="message" id="message" placeholder="Écrire vos commentaires ici." class="textArea"></textarea>
+            </fieldset>
+
+            <input class="submit" type="submit" value="Soumettre">
+        </form>
+    </main>
+
 <?php require 'includes/footer.php'; ?>
